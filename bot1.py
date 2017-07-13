39f4cac0397fa23b2efca2c49bcadf37fb8cbb23
Chris Kansas <ckansas@smcm.edu>
2017-07-13 17:50:12 -0600

    Created a stream listener which can track tweets with words, still need to figure out how to track a specific user and reply to them.

--- bot1.py
+++ bot1.py
@@ -2,7 +2,8 @@
 
 #This will be a twitter bot trial and error - may evolve into something more useful.
 
-import tweepy
+import tweepy, time
+from random import randint
 
 consumer_key = "7j0MW9w0TpO7GzHlOIlUxTyt9"
 consumer_secret = "8J9bTvQUH9gynASVnrq0wJ4TGrZSFxYlzR5nS5jjyOEYjHEjlT"
@@ -13,3 +14,25 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_key, access_secret)
 
 api = tweepy.API(auth)
+
+user_id = "Chris Kansas"
+
+#api.update_status("#HelloWorld - This is JonahBot's first tweet!")
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=myStreamListener)
+
+myStream.filter(track=["dog"])
+
+#for follower in tweepy.Cursor(api.followers).items():
+    #follower.follow()
+    #print(follower.screen_name)
+
+#for i in range(5):
+    #random_number = randint(0, 100)
+    #api.update_status("%s is my favorite number right now." % (random_number))
+    #time.sleep(20)
